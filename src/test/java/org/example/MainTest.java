cf7a4c2ccc14df0abcacdf48b6152774c7524bfb
Hasindu-1 <pabasarahasindu9@gmail.com>
2025-09-16 14:09:07 +0530

    Initial project setup for CI CD

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void sampleTest() {
+        assertEquals(5, 2 + 3, "Simple math test");
+    }
+}
